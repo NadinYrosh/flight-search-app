07247ea8882687f1079c5940a3d008c54a50fb18
Nadiya Yeroshkina <nadin.yeroshkina@gmail.com>
2019-04-01 16:27:04 -0700

    make api call function that writes results into the file

---
+++ src/flight_finder.py
@@ -0,0 +1,26 @@
+import requests
+import json
+import datetime
+from urllib.parse import urlencode
+from os import path
+
+def api_call(fly_from, fly_to, leave_date_from, leave_date_to, return_date_from, return_date_to, currency = 'USD') :
+    # requests.get(" https://api.skypicker.com/flights?flyFrom=PDX&to=LAX&dateFrom=01/04/2019&dateTo=07/04/2019&d=picky")
+    params = {
+        'flyFrom' : fly_from,
+        'to' : fly_to,
+        'dateFrom' : leave_date_from.strftime('%d/%m/%Y'),
+        'dateTo' : leave_date_to.strftime('%d/%m/%Y'),
+        'return_from' : return_date_from.strftime('%d/%m/%Y'),
+        'return_to' : return_date_to.strftime('%d/%m/%Y'),
+        'curr' : currency,
+        'partner' : 'picky'
+    }
+    response = requests.get("https://api.skypicker.com/flights?" + urlencode(params))
+
+    return response.json()
+
+def write_to_file(data, file_path) :
+    file = open(path.expanduser(file_path),'w')
+    file.write(json.dumps(data['data'], sort_keys = True, indent=4))
+    file.close()
