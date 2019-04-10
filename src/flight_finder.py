dc79452864136acf9e4c39c266680db10fca4c1e
Nadiya Yeroshkina <nadin.yeroshkina@gmail.com>
2019-04-10 15:39:12 -0700

    add params to flight duration and layovers

--- src/flight_finder.py
+++ src/flight_finder.py
@@ -4,7 +4,7 @@ import datetime
 from urllib.parse import urlencode
 from os import path
 
-def api_call(fly_from, fly_to, leave_date_from, leave_date_to, return_date_from, return_date_to, currency = 'USD') :
+def api_call(fly_from, fly_to, leave_date_from, leave_date_to, return_date_from, return_date_to, max_fly_duration, max_stopovers, currency = 'USD', adults = 1) :
     # requests.get(" https://api.skypicker.com/flights?flyFrom=PDX&to=LAX&dateFrom=01/04/2019&dateTo=07/04/2019&d=picky")
     params = {
         'flyFrom' : fly_from,
@@ -13,7 +13,10 @@ def api_call(fly_from, fly_to, leave_date_from, leave_date_to, return_date_from,
         'dateTo' : leave_date_to.strftime('%d/%m/%Y'),
         'return_from' : return_date_from.strftime('%d/%m/%Y'),
         'return_to' : return_date_to.strftime('%d/%m/%Y'),
+        'max_fly_duration' : int(max_fly_duration.total_seconds() / 3600),
+        'max_stopovers' : max_stopovers,
         'curr' : currency,
+        'adults' : adults,
         'partner' : 'picky'
     }
     response = requests.get("https://api.skypicker.com/flights?" + urlencode(params))
