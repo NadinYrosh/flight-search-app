c1a1fd169d4cf024e1caaab28568a88f13c22fb4
Nadiya Yeroshkina <nadin.yeroshkina@gmail.com>
2019-05-03 11:22:20 -0700

    update gitignore and an entry point

--- src/flight_finder.py
+++ src/flight_finder.py
@@ -12,7 +12,7 @@ db = sqlite3.connect('my_data.sqlite3')
 def create_schema():
     cursor = db.cursor()
     cursor.execute("""
-        CREATE TABLE trip (
+        CREATE TABLE if not exists trip (
             key text,
             bag_limit integer,
             bag_price integer,
@@ -29,7 +29,7 @@ def create_schema():
     """)
 
     cursor.execute("""
-            CREATE TABLE flight (
+            CREATE TABLE if not exists flight (
             key text,
             airline_key text,
             trip_key text,
@@ -43,37 +43,44 @@ def create_schema():
     """)
 
     cursor.execute("""
-        CREATE TABLE airline (
+        CREATE TABLE if not exists airline (
             key text,
             airline_name text
         );
 
     """)
-
-    
+    db.commit()
 
 
 def api_call(fly_from, fly_to, leave_date_from, leave_date_to, return_date_from, return_date_to, max_fly_duration, max_stopovers, currency = 'USD', adults = 1) :
     # requests.get(" https://api.skypicker.com/flights?flyFrom=PDX&to=LAX&dateFrom=01/04/2019&dateTo=07/04/2019&d=picky")
     params = {
-        'flyFrom':fly_from,
-        'to':fly_to,
-        'dateFrom':leave_date_from.strftime('%d/%m/%Y'),
-        'dateTo':leave_date_to.strftime('%d/%m/%Y'),
-        'return_from':return_date_from.strftime('%d/%m/%Y'),
-        'return_to':return_date_to.strftime('%d/%m/%Y'),
-        'max_fly_duration':int(max_fly_duration.total_seconds() / 3600),
-        'max_stopovers':max_stopovers,
-        'curr':currency,
-        'adults':adults,
-        'partner':'picky'
+        'flyFrom': fly_from,
+        'to': fly_to,
+        'dateFrom': leave_date_from.strftime('%d/%m/%Y'),
+        'dateTo': leave_date_to.strftime('%d/%m/%Y'),
+        'return_from': return_date_from.strftime('%d/%m/%Y'),
+        'return_to': return_date_to.strftime('%d/%m/%Y'),
+        'max_fly_duration': int(max_fly_duration.total_seconds() / 3600),
+        'max_stopovers': max_stopovers,
+        'curr': currency,
+        'adults': adults,
+        'partner': 'picky'
     }
     response = requests.get("https://api.skypicker.com/flights?" + urlencode(params))
 
     return response.json()
 
 
-def write_to_file(data, file_path) :
-    file = open(path.expanduser(file_path),'w')
-    file.write(json.dumps(data['data'], sort_keys = True, indent=4))
+def write_to_file(data, file_path):
+    file = open(path.expanduser(file_path), 'w')
+    file.write(json.dumps(data['data'], sort_keys=True, indent=4))
     file.close()
+
+
+def main():
+    create_schema()
+
+
+if __name__ == '__main__':
+    main()
